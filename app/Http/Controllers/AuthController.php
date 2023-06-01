39d00e5940ed1adb6d2ed3cd9d43cc32ea8ffdfc
LamineDjenaihi <lamine.djenaihi07@yahoo.com>
2023-06-01 20:48:50 +0200

    Create register and Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,10 +1,44 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\LoginUserRequest;
+use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    use HttpResponses;
+
+
+    public function login(LoginUserRequest $request){
+        $request->validated($request->all());
+        if(!Auth::attempt($request->only(['email', 'password']))){
+            return $this->fail('', 'Credentials do not match', 401);
+        }
+        $user = User::where('email', $request->email)->first();
+        return $this->success([
+            'user'=> $user,
+            'token'=> $user->createToken('API Token of'. $user->name)->plainTextToken
+        ]);
+    }
+
+    public function register(StoreUserRequest $request){
+        $request->validated($request->all());
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        return $this->success([
+            'user'=> $user,
+            'token'=> $user->createToken('API Token of'. $user->name)->plainTextToken
+        ]);
+    }
+
+    public function logout(){
+        return response()->json('this is my logout method');
+    }
 }
